1a250f66838d05f92cc8e7dc0b069f7880aa9eb5
AnaPatriciia <anapatriciaoliveira576@gmail.com>
2024-11-12 09:06:06 -0400

    atualização

---
+++ pages/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TELA LOGIN TURMA 140</title>
+</head>
+<body>
+    <h2>TELA LOGIN</h2>
+    <form method="post">
+    <label>Usuário:</label><br>
+    <input type="email" name="email" placeholder="Digite o email do usuario."><br>
+    <label>Senha:</label><br>
+    <input type="password" name="senha" placeholder="********"><br>
+    <input type="submit" value="LOGAR"><br>
+
+    <a href="cadastro.php">Cadastre-se</a>
+    </form>
+</body>
+</html>
